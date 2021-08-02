74be3a2c29c28f3018484edc5e439198822f1b05
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2021-08-02 16:47:37 +0300

    Clone Rotation Fix

--- Assets/Scripts/Cloner/ClonePlayer.cs
+++ Assets/Scripts/Cloner/ClonePlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ClonePlayer : MonoBehaviour
 {
+    public GameObject Player;
     public GameObject CloneText;
     public GameObject playerOriginal;
     public GameObject playerContainer;
@@ -40,27 +41,43 @@ public class ClonePlayer : MonoBehaviour
         {
             if (i < playerNumber)
             {
-                GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + ((i+1) * 0.1f + 2.25f) * 3f, playerOriginal.transform.position.y +3f, playerOriginal.transform.position.z + 1.5f), playerOriginal.transform.rotation);
-                PlayerClone.transform.parent = playerContainer.transform;
-                PlayerClone.name = "PlayerClone" + (i + 1);
+                if(Player.transform.rotation == Quaternion.Euler(0f,0,0))
+                {
+                    GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + ((i+1) * 0.1f + 2.5f) * 3f, playerOriginal.transform.position.y +3f, playerOriginal.transform.position.z + 1.4f), Player.transform.rotation);
+                    PlayerClone.transform.parent = playerContainer.transform;
+                    PlayerClone.name = "PlayerClone" + (i + 1);
+                }
+                else if (Player.transform.rotation == Quaternion.Euler(0f, -90, 0))
+                {
+                    GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + ( 6.5f) * 1f, playerOriginal.transform.position.y + 3f, playerOriginal.transform.position.z + (i + 1) * 0.2f  + 0.7f), Player.transform.rotation);
+                    PlayerClone.transform.parent = playerContainer.transform;
+                    PlayerClone.name = "PlayerClone" + (i + 1);
+                }
+                else if (Player.transform.rotation == Quaternion.Euler(0f, 180, 0))
+                {
+                    GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + ((i + 1) * 0.1f + 2.4f) * 3f, playerOriginal.transform.position.y + 3f, playerOriginal.transform.position.z +0.5f), Player.transform.rotation);
+                    PlayerClone.transform.parent = playerContainer.transform;
+                    PlayerClone.name = "PlayerClone" + (i + 1);
+                }
+
             }
-            else if (i >= playerNumber && i < 2*playerNumber)
-            {
-                GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + 7f, playerOriginal.transform.position.y - 1f, playerOriginal.transform.position.z + ((i - 3) * 0.3f + 1f) * -1.25f), playerOriginal.transform.rotation);
-                PlayerClone.transform.parent = playerContainer.transform;
-                PlayerClone.name = "PlayerClone" + (i + 1);
-            }
-            else
-            {
-                GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + -3.6f, playerOriginal.transform.position.y - 1.6f, playerOriginal.transform.position.z + ((i - 6) * 0.3f + 1f) * -1.25f), playerOriginal.transform.rotation);
-                PlayerClone.transform.parent = playerContainer.transform;
-                PlayerClone.name = "PlayerClone" + (i + 1);
-            }
+            //else if (i >= playerNumber && i < 2*playerNumber)
+            //{
+            //    GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + 7f, playerOriginal.transform.position.y - 1f, playerOriginal.transform.position.z + ((i - 3) * 0.3f + 1f) * -1.25f), playerOriginal.transform.rotation);
+            //    PlayerClone.transform.parent = playerContainer.transform;
+            //    PlayerClone.name = "PlayerClone" + (i + 1);
+            //}
+            //else
+            //{
+            //    GameObject PlayerClone = Instantiate(playerOriginal, new Vector3(playerOriginal.transform.position.x + -3.6f, playerOriginal.transform.position.y - 1.6f, playerOriginal.transform.position.z + ((i - 6) * 0.3f + 1f) * -1.25f), playerOriginal.transform.rotation);
+            //    PlayerClone.transform.parent = playerContainer.transform;
+            //    PlayerClone.name = "PlayerClone" + (i + 1);
+            //}
         }
         totalNumOfPlayers = numOfPlayers + i;
         cloned = true;
         Destroy(CloneText);
-        playerContainer.transform.Rotate(0,-90,0);
+        //playerContainer.transform.Rotate(0,-90,0);
 
     }
 
