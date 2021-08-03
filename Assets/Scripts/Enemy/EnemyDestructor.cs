f920783e83c267983f816b63369acaafc4240756
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2021-08-03 22:39:27 +0300

    End Screen UI

--- Assets/Scripts/Enemy/EnemyDestructor.cs
+++ Assets/Scripts/Enemy/EnemyDestructor.cs
@@ -9,15 +9,15 @@ public class EnemyDestructor : MonoBehaviour
     GameObject a;
     GameObject b;
 
-    int totalNumOfPlayers;
-    int totalNumOfEnemies;
-    //public float destroyTime = 1f;
-    //int i = 0;
+    public int totalNumOfPlayers;
+    public int totalNumOfEnemies;
+
 
     float xdirection;
     float zdirection;
 
     bool isDestroyed;
+    public bool GameOver;
 
     void Awake()
     {
@@ -29,27 +29,22 @@ public class EnemyDestructor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        
-        
-
+       
     }
 
     void OnTriggerEnter()
     {
         if(!isDestroyed)
-            {
-
-           
+        {          
         totalNumOfPlayers = a.GetComponent<ClonePlayer>().totalNumOfPlayers;
         totalNumOfEnemies = b.GetComponent<EnemyCreator>().totalNumOfEnemies;
+
+
         StartCoroutine(DestroyEnemies());
        
-
-
-
         IEnumerator DestroyEnemies()
         {
+            
             var Enemies = new List<GameObject>();
             foreach (Transform child in EnemyContainer.transform) Enemies.Add(child.gameObject);
 
@@ -57,8 +52,8 @@ public class EnemyDestructor : MonoBehaviour
             {
                 for (int i = 0; i < totalNumOfEnemies; i++)
                 {
-                        SphereCollider sphereCollider = Enemies[i].GetComponent<SphereCollider>();
-                        Destroy(sphereCollider);
+                    SphereCollider sphereCollider = Enemies[i].GetComponent<SphereCollider>();
+                    Destroy(sphereCollider);
                     xdirection = Random.Range(-180, 180);
                     zdirection = Random.Range(-90, 270);
 
@@ -68,16 +63,16 @@ public class EnemyDestructor : MonoBehaviour
                     r.AddForce(new Vector3(xdirection,90,zdirection));
                     yield return new WaitForSeconds(0.7f);
                     Destroy(Enemies[i]);
-                  
+                    isDestroyed = true;
                 }
             }
             else
             {
                 for (int i = 0; i < totalNumOfPlayers; i++)
                 {
-                        SphereCollider sphereCollider = Enemies[i].GetComponent<SphereCollider>();
-                        Destroy(sphereCollider);
-                        xdirection = Random.Range(-180, 180);
+                    SphereCollider sphereCollider = Enemies[i].GetComponent<SphereCollider>();
+                    Destroy(sphereCollider);
+                    xdirection = Random.Range(-180, 180);
                     zdirection = Random.Range(-90, 2700);
 
                     Enemies[i].transform.Rotate(90, 0, 0);
@@ -86,19 +81,30 @@ public class EnemyDestructor : MonoBehaviour
                     r.AddForce(new Vector3(xdirection, 90, zdirection));
                     yield return new WaitForSeconds(0.7f);
                     Destroy(Enemies[i]);
+                    isDestroyed = true;
                 }
             }
-                isDestroyed = true;
-        }
 
-        }
+            if (isDestroyed)
+            {
+                GameOver = true;
+            }
+             
 
-    }
+
+            } 
+        }
         
-        
+
+                   
+
+        }    
     // Update is called once per frame
     void Update()
     {
         
     }
 }
+        
+    
+
